a3d3a3a0cb6e1a5c92a1ef4259c6251750148b16
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-04-12 14:34:45 -0400

    refact: some refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,39 @@
-import ts from "typescript";
+import * as ts from "typescript";
 import { dirname, resolve, relative } from "path";
 
 const transformer = <T extends ts.Node>(_: ts.Program) => {
   return (context: ts.TransformationContext) => (rootNode: T) => {
-    if (!is<ts.SourceFile>(rootNode, ts.SyntaxKind.SourceFile)) {
-      return rootNode;
-    }
     const compilerOptions = context.getCompilerOptions();
     // TODO should check if baseUrl and paths are defined
     const baseUrl = compilerOptions.baseUrl!;
     const paths = compilerOptions.paths!;
-    const fileDir = dirname(rootNode.fileName);
     const regPaths = Object.keys(paths).map(key => ({
       regexp: new RegExp("^" + key.replace("*", "(.*)") + "$"),
       resolve: paths[key][0] // TODO should check if is not empty
     }));
+    let fileDir = "";
     function visit(node: ts.Node): ts.Node {
-      if (is<ts.ImportDeclaration>(node, ts.SyntaxKind.ImportDeclaration)) {
-        if (
-          is<ts.StringLiteral>(
-            node.moduleSpecifier,
-            ts.SyntaxKind.StringLiteral
-          )
-        ) {
-          for (const path of regPaths) {
-            const match = node.moduleSpecifier.text.match(path.regexp);
-            if (match) {
-              const out = path.resolve.replace(/\*/g, match[1]);
-              const file = relative(fileDir, resolve(baseUrl, out));
-              // If it's in the same level or below add the ./
-              node.moduleSpecifier.text = file[0] === "." ? file : `./${file}`;
-              break;
-            }
-          }
+      if (ts.isSourceFile(node)) {
+        fileDir = dirname(node.fileName);
+        return ts.visitEachChild(node, visit, context);
+      }
+      if (
+        ts.isImportDeclaration(node) &&
+        ts.isStringLiteral(node.moduleSpecifier)
+      ) {
+        for (const path of regPaths) {
+          const match = node.moduleSpecifier.text.match(path.regexp);
+          if (match === null) continue;
+          const out = path.resolve.replace(/\*/g, match[1]);
+          const file = relative(fileDir, resolve(baseUrl, out));
+          return ts.updateImportDeclaration(
+            node,
+            node.decorators,
+            node.modifiers,
+            node.importClause,
+            // If it's in the same level or below add the ./
+            ts.createLiteral(file[0] === "." ? file : `./${file}`)
+          );
         }
       }
       return ts.visitEachChild(node, visit, context);
@@ -40,9 +41,5 @@ const transformer = <T extends ts.Node>(_: ts.Program) => {
     return ts.visitNode(rootNode, visit);
   };
 };
-
-function is<T extends ts.Node>(node: ts.Node, kind: ts.SyntaxKind): node is T {
-  return node.kind === kind;
-}
 
 export default transformer;
