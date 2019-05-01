546e610e716ee619c988cc7a59ae9fdda34ad9d7
Михайлов Антон <anion155@gmail.com>
2019-05-01 15:22:46 +0500

    feat: exports expressions
    closes #7

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,17 @@ const transformer = <T extends ts.Node>(_: ts.Program) => {
       resolve: paths[key][0] // TODO should check if is not empty
     }));
     let fileDir = "";
+    function findFileInPaths(text: string) {
+      for (const path of regPaths) {
+        const match = text.match(path.regexp);
+        if (match) {
+          const out = path.resolve.replace(/\*/g, match[1]);
+          const file = slash(relative(fileDir, resolve(baseUrl, out)));
+          return file;
+        }
+      }
+      return null;
+    }
     function visit(node: ts.Node): ts.Node {
       if (ts.isSourceFile(node)) {
         fileDir = dirname(node.fileName);
@@ -22,16 +33,30 @@ const transformer = <T extends ts.Node>(_: ts.Program) => {
         ts.isImportDeclaration(node) &&
         ts.isStringLiteral(node.moduleSpecifier)
       ) {
-        for (const path of regPaths) {
-          const match = node.moduleSpecifier.text.match(path.regexp);
-          if (match === null) continue;
-          const out = path.resolve.replace(/\*/g, match[1]);
-          const file = slash(relative(fileDir, resolve(baseUrl, out)));
+        const file = findFileInPaths(node.moduleSpecifier.text);
+        if (file) {
           return ts.updateImportDeclaration(
             node,
             node.decorators,
             node.modifiers,
             node.importClause,
+            // If it's in the same level or below add the ./
+            ts.createLiteral(file[0] === "." ? file : `./${file}`)
+          );
+        }
+      }
+      if (
+        ts.isExportDeclaration(node) &&
+        node.moduleSpecifier &&
+        ts.isStringLiteral(node.moduleSpecifier)
+      ) {
+        const file = findFileInPaths(node.moduleSpecifier.text);
+        if (file) {
+          return ts.updateExportDeclaration(
+            node,
+            node.decorators,
+            node.modifiers,
+            node.exportClause,
             // If it's in the same level or below add the ./
             ts.createLiteral(file[0] === "." ? file : `./${file}`)
           );
---
+++ tests/utils/index.ts
@@ -0,0 +1,2 @@
+export * from '@utils/sum';
+export * from '@utils/subs';
