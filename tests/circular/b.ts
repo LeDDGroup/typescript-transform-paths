46eeacb4ba550453e6496055728cbf7c23f898c0
Mikhaylov Anton <anion155@gmail.com>
2019-05-17 18:17:57 +0500

    Add test for circular depency error #9

---
+++ tests/circular/b.ts
@@ -0,0 +1,7 @@
+import { A } from '@circular/a';
+
+export class B {
+  print(a: A) {
+    console.log(a.name);
+  }
+}
--- tests/core/index.ts
+++ tests/core/index.ts
@@ -1,4 +1,7 @@
 import { sum, subs } from '@utils';
+import { B } from '@circular/b';
+import { A } from '@circular/a';
 
 sum(2, 3);
 subs(2, 3);
+const a = new A('');
