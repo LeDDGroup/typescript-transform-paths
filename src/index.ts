c3710c461ea5657d4496eba73a1bc5d0a1715a6e
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-05-17 21:34:44 -0400

    fix: check for `paths` and `baseUrl` in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
+import { dirname, relative, resolve } from "path";
 import * as ts from "typescript";
-import { dirname, resolve, relative } from "path";
 import slash = require("slash");
 
 const transformer = <T extends ts.Node>(_: ts.Program) => {
   return (context: ts.TransformationContext) => (rootNode: T) => {
     const compilerOptions = context.getCompilerOptions();
-    // TODO should check if baseUrl and paths are defined
-    const baseUrl = compilerOptions.baseUrl!;
-    const paths = compilerOptions.paths!;
-    const regPaths = Object.keys(paths).map(key => ({
-      regexp: new RegExp("^" + key.replace("*", "(.*)") + "$"),
-      resolve: paths[key][0] // TODO should check if is not empty
-    }));
+    if (
+      compilerOptions.baseUrl === undefined ||
+      compilerOptions.paths === undefined
+    ) {
+      throw new Error(
+        "Should define baseUrl and paths properties in the tsconfig"
+      );
+    }
+    const baseUrl = compilerOptions.baseUrl;
+    const paths = compilerOptions.paths;
+    const regPaths = Object.keys(paths)
+      .map(key => ({
+        regexp: new RegExp("^" + key.replace("*", "(.*)") + "$"),
+        resolve: paths[key][0]
+      }));
     let fileDir = "";
     function findFileInPaths(text: string) {
       for (const path of regPaths) {
