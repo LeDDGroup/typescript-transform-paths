aa7eb168dfb31539a0cb9cd203595140c5644509
Михайлов Антон <anion155@gmail.com>
2019-05-21 11:40:38 +0500

    Filter import and export declarations

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,130 @@
-import { dirname, relative, resolve } from "path";
-import * as ts from "typescript";
-import slash = require("slash");
 
-const transformer = <T extends ts.Node>(_: ts.Program) => {
-  return (context: ts.TransformationContext) => (rootNode: T) => {
-    const compilerOptions = context.getCompilerOptions();
+import * as ts from 'typescript';
+import * as fs from 'fs';
+import { dirname, relative, join } from 'path';
+
+interface PluginConfig {
+  extensions?: (string | [string, string])[];
+}
+
+const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+  const resolver = (context as any).getEmitResolver();
+  const compilerOptions = context.getCompilerOptions();
+  const sourceDir = dirname(sourceFile.fileName);
+
+  const { baseUrl = '', paths = { } } = compilerOptions;
+  const { extensions = [['.ts', '.js'], ['.tsx', '.jsx'], '.js', '.jsx'] } = config || { };
+
+  const binds = Object.keys(paths).map(key => ({
+    regexp: new RegExp(`^${key.replace(/\*$/, '(.*)')}$`),
+    paths: paths[key],
+  }));
+
+  if (!baseUrl || binds.length === 0) {
+    // There is nothing we can do without baseUrl and paths specified.
+    return sourceFile;
+  }
+
+  function findModuleFile(moduleName: string) {
+    for (const { regexp, paths } of binds) {
+      const match = regexp.exec(moduleName);
+      if (match) {
+        for (const path of paths) {
+          const dir = join(baseUrl, path.replace(/\*$/, match[1]));
+          if (fs.existsSync(dir)) {
+            return dir;
+          }
+          for (let ext of extensions) {
+            let src = ext;
+            if (Array.isArray(ext)) {
+              src = ext[0];
+              ext = ext[1];
+            }
+            if (fs.existsSync(`${dir}${src}`)) {
+              return `${dir}${ext}`;
+            }
+          }
+        }
+      }
+    }
+    return undefined;
+  }
+  function bindModuleToFile(moduleName: string) {
+    let file = findModuleFile(moduleName);
+    if (!file) {
+      return undefined;
+    }
+
+    file = relative(sourceDir, file);
+    return file[0] === '.' ? file : './' + file;
+  }
+
+  function visit(node: ts.Node): ts.VisitResult<ts.Node> {
+    if (ts.isImportDeclaration(node)) {
+      return unpathImportDeclaration(node);
+    }
+    if (ts.isExportDeclaration(node)) {
+      return unpathExportDeclaration(node);
+    }
+
+    return ts.visitEachChild(node, visit, context);
+  }
+
+  function unpathImportDeclaration(node: ts.ImportDeclaration): ts.VisitResult<ts.Statement> {
+    if (!ts.isStringLiteral(node.moduleSpecifier)) {
+      return node;
+    }
+    const file = bindModuleToFile(node.moduleSpecifier.text);
+    if (!file) {
+      return node;
+    }
+    const fileLiteral = ts.createLiteral(file);
+
+    const importClause = ts.visitNode(node.importClause, visitImportClause as any, ts.isImportClause);
+    return importClause ? (
+      ts.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, fileLiteral)
+    ) : undefined;
+  }
+  function visitImportClause(node: ts.ImportClause): ts.VisitResult<ts.ImportClause> {
+    const name = resolver.isReferencedAliasDeclaration(node) ? node.name : undefined;
+    const namedBindings = ts.visitNode(node.namedBindings, visitNamedImportBindings as any, ts.isNamedImports);
+    return (name || namedBindings) ? ts.updateImportClause(node, name, namedBindings) : undefined;
+  }
+  function visitNamedImportBindings(node: ts.NamedImportBindings): ts.VisitResult<ts.NamedImportBindings> {
+    if (node.kind === ts.SyntaxKind.NamespaceImport) {
+      return resolver.isReferencedAliasDeclaration(node) ? node : undefined;
+    }
+    else {
+      const elements = ts.visitNodes(node.elements, visitImportSpecifier as any, ts.isImportSpecifier);
+      return elements.some(e => e) ? ts.updateNamedImports(node, elements) : undefined;
+    }
+  }
+  function visitImportSpecifier(node: ts.ImportSpecifier): ts.VisitResult<ts.ImportSpecifier> {
+    return resolver.isReferencedAliasDeclaration(node) ? node : undefined;
+  }
+
+  function unpathExportDeclaration(node: ts.ExportDeclaration): ts.VisitResult<ts.Statement> {
+    if (!node.moduleSpecifier || !ts.isStringLiteral(node.moduleSpecifier)) {
+      return node;
+    }
     if (
-      compilerOptions.baseUrl === undefined ||
-      compilerOptions.paths === undefined
+      !node.exportClause
+      && !compilerOptions.isolatedModules
+      && !resolver.moduleExportsSomeValue(node.moduleSpecifier)
     ) {
-      throw new Error(
-        "Should define baseUrl and paths properties in the tsconfig"
-      );
+      return undefined;
     }
-    const baseUrl = compilerOptions.baseUrl;
-    const paths = compilerOptions.paths;
-    const regPaths = Object.keys(paths)
-      .map(key => ({
-        regexp: new RegExp("^" + key.replace("*", "(.*)") + "$"),
-        resolve: paths[key][0]
-      }));
-    let fileDir = "";
-    function findFileInPaths(text: string) {
-      for (const path of regPaths) {
-        const match = text.match(path.regexp);
-        if (match) {
-          const out = path.resolve.replace(/\*/g, match[1]);
-          const file = slash(relative(fileDir, resolve(baseUrl, out)));
-          return file[0] === "." ? file : `./${file}`;
-        }
-      }
-      return null;
+
+    const file = bindModuleToFile(node.moduleSpecifier.text);
+    if (!file) {
+      return node;
     }
-    function visit(node: ts.Node): ts.Node {
-      if (ts.isSourceFile(node)) {
-        fileDir = dirname(node.fileName);
-        return ts.visitEachChild(node, visit, context);
-      }
-      if (
-        (ts.isImportDeclaration(node) || ts.isExportDeclaration(node)) &&
-        node.moduleSpecifier &&
-        ts.isStringLiteral(node.moduleSpecifier)
-      ) {
-        const file = findFileInPaths(node.moduleSpecifier.text);
-        if (file) {
-          node.moduleSpecifier.text = file;
-          return node;
-        }
-      }
-      return ts.visitEachChild(node, visit, context);
-    }
-    return ts.visitNode(rootNode, visit);
-  };
+    const fileLiteral = ts.createLiteral(file);
+
+    return ts.updateExportDeclaration(node, node.decorators, node.modifiers, node.exportClause, fileLiteral);
+  }
+
+  return ts.visitNode(sourceFile, visit);
 };
 
 export default transformer;
