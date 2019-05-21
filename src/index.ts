2a012c704d549cedfd5a99af4d217615bebf8781
Mikhaylov Anton <anion155@gmail.com>
2019-05-21 11:46:43 +0500

    Formating

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import * as ts from "typescript";
 import * as fs from "fs";
 import { dirname, relative, join } from "path";
+import * as slash from "slash";
 
 interface PluginConfig {
   extensions?: (string | [string, string])[];
 }
 
-const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+const transformer = (
+  _: ts.Program, config?: PluginConfig
+) => (
+  context: ts.TransformationContext
+) => (
+  sourceFile: ts.SourceFile
+) => {
   const resolver = (context as any).getEmitResolver();
   const compilerOptions = context.getCompilerOptions();
   const sourceDir = dirname(sourceFile.fileName);
 
   const { baseUrl = "", paths = { } } = compilerOptions;
-  const { extensions = [[".ts", ".js"], [".tsx", ".jsx"], ".js", ".jsx"] } = config || { };
+  const { extensions = [
+    [".ts", ".js"], [".tsx", ".jsx"], ".js", ".jsx"
+  ] } = config || { };
 
   const binds = Object.keys(paths).map(key => ({
     regexp: new RegExp(`^${key.replace(/\*$/, "(.*)")}$`),
@@ -54,7 +63,7 @@ const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.Trans
       return undefined;
     }
 
-    file = relative(sourceDir, file);
+    file = slash(relative(sourceDir, file));
     return file[0] === "." ? file : "./" + file;
   }
 
@@ -69,7 +78,9 @@ const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.Trans
     return ts.visitEachChild(node, visit, context);
   }
 
-  function unpathImportDeclaration(node: ts.ImportDeclaration): ts.VisitResult<ts.Statement> {
+  function unpathImportDeclaration(
+    node: ts.ImportDeclaration
+  ): ts.VisitResult<ts.Statement> {
     if (!ts.isStringLiteral(node.moduleSpecifier)) {
       return node;
     }
@@ -79,39 +90,73 @@ const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.Trans
     }
     const fileLiteral = ts.createLiteral(file);
 
-    const importClause = ts.visitNode(node.importClause, visitImportClause as any, ts.isImportClause);
-    return importClause ? (
-      ts.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, fileLiteral)
+    const importClause = ts.visitNode(
+      node.importClause,
+      visitImportClause as any,
+      ts.isImportClause
+    );
+    return importClause ? ts.updateImportDeclaration(
+      node,
+      node.decorators,
+      node.modifiers,
+      node.importClause,
+      fileLiteral
     ) : undefined;
   }
-  function visitImportClause(node: ts.ImportClause): ts.VisitResult<ts.ImportClause> {
-    const name = resolver.isReferencedAliasDeclaration(node) ? node.name : undefined;
-    const namedBindings = ts.visitNode(node.namedBindings, visitNamedImportBindings as any, ts.isNamedImports);
-    return (name || namedBindings) ? ts.updateImportClause(node, name, namedBindings) : undefined;
+  function visitImportClause(
+    node: ts.ImportClause
+  ): ts.VisitResult<ts.ImportClause> {
+    const name = resolver.isReferencedAliasDeclaration(node)
+      ? node.name : undefined;
+    const namedBindings = ts.visitNode(
+      node.namedBindings,
+      visitNamedImportBindings as any,
+      ts.isNamedImports
+    );
+    return (name || namedBindings)
+      ? ts.updateImportClause(node, name, namedBindings)
+      : undefined;
   }
-  function visitNamedImportBindings(node: ts.NamedImportBindings): ts.VisitResult<ts.NamedImportBindings> {
+  function visitNamedImportBindings(
+    node: ts.NamedImportBindings
+  ): ts.VisitResult<ts.NamedImportBindings> {
     if (node.kind === ts.SyntaxKind.NamespaceImport) {
-      return resolver.isReferencedAliasDeclaration(node) ? node : undefined;
+      return resolver.isReferencedAliasDeclaration(node)
+        ? node : undefined;
     }
     else {
-      const elements = ts.visitNodes(node.elements, visitImportSpecifier as any, ts.isImportSpecifier);
-      return elements.some(e => e) ? ts.updateNamedImports(node, elements) : undefined;
+      const elements = ts.visitNodes(
+        node.elements,
+        visitImportSpecifier as any,
+        ts.isImportSpecifier
+      );
+      return elements.some(e => e)
+        ? ts.updateNamedImports(node, elements)
+        : undefined;
     }
   }
-  function visitImportSpecifier(node: ts.ImportSpecifier): ts.VisitResult<ts.ImportSpecifier> {
-    return resolver.isReferencedAliasDeclaration(node) ? node : undefined;
+  function visitImportSpecifier(
+    node: ts.ImportSpecifier
+  ): ts.VisitResult<ts.ImportSpecifier> {
+    return resolver.isReferencedAliasDeclaration(node)
+      ? node : undefined;
   }
 
-  function unpathExportDeclaration(node: ts.ExportDeclaration): ts.VisitResult<ts.Statement> {
-    if (!node.moduleSpecifier || !ts.isStringLiteral(node.moduleSpecifier)) {
-      return node;
-    }
+  function unpathExportDeclaration(
+    node: ts.ExportDeclaration
+  ): ts.VisitResult<ts.Statement> {
     if (
       !node.exportClause &&
       !compilerOptions.isolatedModules &&
       !resolver.moduleExportsSomeValue(node.moduleSpecifier)
     ) {
       return undefined;
+    }
+    if (node.exportClause && resolver.isValueAliasDeclaration(node)) {
+      return undefined;
+    }
+    if (!node.moduleSpecifier || !ts.isStringLiteral(node.moduleSpecifier)) {
+      return node;
     }
 
     const file = bindModuleToFile(node.moduleSpecifier.text);
@@ -120,7 +165,38 @@ const transformer = (_: ts.Program, config?: PluginConfig) => (context: ts.Trans
     }
     const fileLiteral = ts.createLiteral(file);
 
-    return ts.updateExportDeclaration(node, node.decorators, node.modifiers, node.exportClause, fileLiteral);
+    const exportClause = ts.visitNode(
+      node.exportClause,
+      visitNamedExports as any,
+      ts.isNamedExports
+    );
+    return node.exportClause === exportClause || exportClause ? (
+      ts.updateExportDeclaration(
+        node,
+        node.decorators,
+        node.modifiers,
+        node.exportClause,
+        fileLiteral
+      )
+    ) : undefined;
+  }
+  function visitNamedExports(
+    node: ts.NamedExports
+  ): ts.VisitResult<ts.NamedExports> {
+    const elements = ts.visitNodes(
+      node.elements,
+      visitExportSpecifier as any,
+      ts.isExportSpecifier
+    );
+    return elements.some(e => e)
+      ? ts.updateNamedExports(node, elements)
+      : undefined;
+  }
+  function visitExportSpecifier(
+    node: ts.ExportSpecifier
+  ): ts.VisitResult<ts.ExportSpecifier> {
+    return resolver.isValueAliasDeclaration(node)
+      ? node : undefined;
   }
 
   return ts.visitNode(sourceFile, visit);
