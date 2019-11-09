1e936b89fc7de1f52b1272833f5ccd3fa177923c
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-11-09 17:56:49 -0500

    fix: handle tsconfig with out paths (#41)
    * test: tsconfig without paths should work
    
    * fix: handle tsconfig with out paths
    
    * ci: fix appveyor

--- tests/index.ts
+++ tests/index.ts
@@ -3,27 +3,54 @@ import slash = require("slash");
 import read = require("fs-readdir-recursive");
 import { readFileSync } from "fs";
 
-const root = join(__dirname, "__result/original");
-const files = read(root);
-files.forEach(file => {
-  test(file, () => {
-    const originalFile = join(__dirname, "__result/original", file);
-    const sourceDir = dirname(relative(root, originalFile));
-    const original = update(readFileSync(originalFile, "utf8"), sourceDir);
-    const generatedFile = join(__dirname, "__result/generated", file);
-    const generated = readFileSync(generatedFile, "utf8");
-    expect(generated).toEqual(original);
+describe("with-path", () => {
+  const root = join(__dirname, "__result/with-path/original");
+  const files = read(root);
+  files.forEach(file => {
+    test(file, () => {
+      const originalFile = join(__dirname, "__result/with-path/original", file);
+      const sourceDir = dirname(relative(root, originalFile));
+      const original = readFileSync(originalFile, "utf8")
+        .replace(/"(@.*)"/g, (_, moduleName) => {
+          return `"${bindModuleToFile(moduleName, sourceDir)}"`;
+        })
+        .replace('"path"', '"https://external.url/path.js"')
+        .replace('"circular/a"', '"../circular/a"');
+      const generatedFile = join(
+        __dirname,
+        "__result/with-path/generated",
+        file
+      );
+      const generated = readFileSync(generatedFile, "utf8");
+      expect(generated).toEqual(original);
+    });
   });
 });
 
-function update(content: string, sourceDir: string) {
-  return content
-    .replace(/"(@.*)"/g, (_, moduleName) => {
-      return `"${bindModuleToFile(moduleName, sourceDir)}"`;
-    })
-    .replace('"path"', '"https://external.url/path.js"')
-    .replace('"circular/a"', '"../circular/a"');
-}
+describe("without-path", () => {
+  const root = join(__dirname, "__result/without-path/original");
+  const files = read(root);
+  files.forEach(file => {
+    test(file, () => {
+      const originalFile = join(
+        __dirname,
+        "__result/without-path/original",
+        file
+      );
+      const original = readFileSync(originalFile, "utf8").replace(
+        '"utils/logger"',
+        '"../utils/logger"'
+      );
+      const generatedFile = join(
+        __dirname,
+        "__result/without-path/generated",
+        file
+      );
+      const generated = readFileSync(generatedFile, "utf8");
+      expect(generated).toEqual(original);
+    });
+  });
+});
 
 function bindModuleToFile(moduleName: string, sourceDir: string) {
   const match = /@(.*)/.exec(moduleName);
