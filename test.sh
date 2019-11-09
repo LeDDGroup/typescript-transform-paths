4e52e3bf99c594ee223fdcc0e67dcc5259672e71
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-11-09 17:54:04 -0500

    ci: fix appveyor

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-npx tsc -p tests/__fixtures/with-path/tsconfig.json --outDir tests/__result/with-path/original
-npx ttsc -p tests/__fixtures/with-path/tsconfig.json --outDir tests/__result/with-path/generated
-npx tsc -p tests/__fixtures/without-path/tsconfig.json --outDir tests/__result/without-path/original
-npx ttsc -p tests/__fixtures/without-path/tsconfig.json --outDir tests/__result/without-path/generated
-npx jest
