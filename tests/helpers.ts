59e1f5b5c8631cd945c17bd050f1f081455e590d
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-08-03 21:31:49 -0400

    more formatting

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,14 +1,12 @@
-import path from 'path';
-import * as TS from 'typescript';
-import { TsTransformPathsConfig } from '../src';
-
+import path from "path";
+import * as TS from "typescript";
+import { TsTransformPathsConfig } from "../src";
 
 /* ****************************************************************************************************************** *
  * Config
  * ****************************************************************************************************************** */
 
-const ts = require('ttypescript') as typeof TS;
-
+const ts = require("ttypescript") as typeof TS;
 
 /* ****************************************************************************************************************** *
  * Test Helpers
@@ -22,7 +20,7 @@ export function createProgram(
   disablePlugin?: boolean,
   additionalOptions?: TS.CompilerOptions,
   pluginOptions?: TsTransformPathsConfig
-): TS.Program
+): TS.Program;
 /**
  * Create TS Program with real tsConfigFile
  */
@@ -31,36 +29,49 @@ export function createProgram(
   disablePlugin?: boolean,
   additionalOptions?: TS.CompilerOptions,
   pluginOptions?: TsTransformPathsConfig
-): TS.Program
+): TS.Program;
 export function createProgram(
   configOrFiles: string | { [fileName: string]: /* data */ string },
   disablePlugin?: boolean,
   additionalOptions?: TS.CompilerOptions,
   pluginOptions?: TsTransformPathsConfig
 ): TS.Program {
-  const transformerPath = path.resolve(__dirname, '../src/index.ts');
+  const transformerPath = path.resolve(__dirname, "../src/index.ts");
 
   const extendOptions = Object.assign({}, additionalOptions, {
     outDir: undefined,
     noEmit: false,
-    plugins: disablePlugin ? [] : [
-      { 'transform': transformerPath, ...pluginOptions },
-      { 'transform': transformerPath, 'afterDeclarations': true, ...pluginOptions }
-    ]
+    plugins: disablePlugin
+      ? []
+      : [
+          { transform: transformerPath, ...pluginOptions },
+          {
+            transform: transformerPath,
+            afterDeclarations: true,
+            ...pluginOptions,
+          },
+        ],
   });
 
   let options: any = {};
   let fileNames: string[];
   let host: TS.CompilerHost | undefined;
-  if (typeof configOrFiles === 'string') {
-    const pcl = ts.getParsedCommandLineOfConfigFile(configOrFiles, extendOptions, (<any>ts.sys))!;
+  if (typeof configOrFiles === "string") {
+    const pcl = ts.getParsedCommandLineOfConfigFile(
+      configOrFiles,
+      extendOptions,
+      <any>ts.sys
+    )!;
     options = pcl.options;
     fileNames = pcl.fileNames;
   } else {
-    const files = Object.entries(configOrFiles).reduce((p, [ fileName, data ]) => {
-      p[ts.normalizePath(fileName)] = data;
-      return p
-    }, <any>{});
+    const files = Object.entries(configOrFiles).reduce(
+      (p, [fileName, data]) => {
+        p[ts.normalizePath(fileName)] = data;
+        return p;
+      },
+      <any>{}
+    );
     fileNames = Object.keys(files);
 
     host = ts.createCompilerHost(options);
@@ -68,16 +79,20 @@ export function createProgram(
 
     /* Patch host to feed mock files */
     const originalGetSourceFile: any = host.getSourceFile;
-    host.getSourceFile = function (fileName: string, scriptTarget: TS.ScriptTarget) {
-      if (Object.keys(files).includes(fileName)) return ts.createSourceFile(fileName, files[fileName], scriptTarget);
+    host.getSourceFile = function (
+      fileName: string,
+      scriptTarget: TS.ScriptTarget
+    ) {
+      if (Object.keys(files).includes(fileName))
+        return ts.createSourceFile(fileName, files[fileName], scriptTarget);
       else originalGetSourceFile.apply(undefined, arguments);
-    }
+    };
   }
 
   return ts.createProgram({ options, rootNames: fileNames, host });
 }
 
-export type EmittedFiles = { [fileName: string]: { js: string, dts: string } }
+export type EmittedFiles = { [fileName: string]: { js: string; dts: string } };
 
 /**
  * Get emitted files for program
@@ -90,10 +105,10 @@ export function getEmitResult(program: TS.Program): EmittedFiles {
     let { 1: rootName, 2: ext } = fileName.match(/(.+)\.((d.ts)|(js))$/) ?? [];
     if (!ext) return;
     rootName = `${rootName}.ts`;
-    const key = ext.replace('.', '') as keyof EmittedFiles[string];
+    const key = ext.replace(".", "") as keyof EmittedFiles[string];
     if (!outputFiles[rootName]) outputFiles[rootName] = <any>{};
     outputFiles[rootName][key] = data;
-  }
+  };
 
   program.emit(undefined, writeFile);
 
