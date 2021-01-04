b1fdb545c2d963cd4d82a6a0bedfa2d7d0107398
Ron S <ron@nonara.com>
2021-01-03 21:06:45 -0500

    feat: Added `exclude` option to allow excluding transformation of matching resolved paths

--- test/tests/transformer/specific.test.ts
+++ test/tests/transformer/specific.test.ts
@@ -3,6 +3,7 @@ import {} from "ts-expose-internals";
 import * as path from "path";
 import { createTsProgram, EmittedFiles, getEmitResult } from "../../utils";
 import { projectsPaths, ts, tsModules, tTypeScript } from "../config";
+import { TsTransformPathsConfig } from "../../../src/types";
 
 /* ****************************************************************************************************************** *
  * Config
@@ -23,6 +24,7 @@ describe(`Transformer -> Specific Cases`, () => {
   const srcFile = ts.normalizePath(path.join(projectRoot, "src/dir/src-file.ts"));
   const indexFile = ts.normalizePath(path.join(projectRoot, "src/index.ts"));
   const typeElisionIndex = ts.normalizePath(path.join(projectRoot, "src/type-elision/index.ts"));
+  const baseConfig: TsTransformPathsConfig = { exclude: ["**/excluded/**", "excluded-file.*"] };
 
   describe.each(testTsModules)(`TypeScript %s`, (s, tsInstance) => {
     let rootDirsEmit: EmittedFiles;
@@ -30,10 +32,24 @@ describe(`Transformer -> Specific Cases`, () => {
     const tsVersion = +tsInstance.versionMajorMinor.split(".").slice(0, 2).join("");
 
     beforeAll(() => {
-      const program = createTsProgram({ tsInstance, tsConfigFile, pluginOptions: { useRootDirs: false } });
+      const program = createTsProgram({
+        tsInstance,
+        tsConfigFile,
+        pluginOptions: {
+          ...baseConfig,
+          useRootDirs: false,
+        },
+      });
       normalEmit = getEmitResult(program);
 
-      const rootDirsProgram = createTsProgram({ tsInstance, tsConfigFile, pluginOptions: { useRootDirs: true } });
+      const rootDirsProgram = createTsProgram({
+        tsInstance,
+        tsConfigFile,
+        pluginOptions: {
+          ...baseConfig,
+          useRootDirs: true,
+        },
+      });
       rootDirsEmit = getEmitResult(rootDirsProgram);
     });
 
@@ -42,6 +58,56 @@ describe(`Transformer -> Specific Cases`, () => {
       expect(rootDirsEmit[srcFile].dts).toMatch(`import "./gen-file"`);
       expect(rootDirsEmit[indexFile].dts).toMatch(`export { B } from "./dir/gen-file"`);
       expect(rootDirsEmit[indexFile].dts).toMatch(`export { A } from "./dir/src-file"`);
+    });
+
+    test(`(useRootDirs: false) Ignores rootDirs`, () => {
+      expect(normalEmit[genFile].dts).toMatch(`import "../../src/dir/src-file"`);
+      expect(normalEmit[srcFile].dts).toMatch(`import "../../generated/dir/gen-file"`);
+      expect(normalEmit[indexFile].dts).toMatch(`export { B } from "../generated/dir/gen-file"`);
+      expect(normalEmit[indexFile].dts).toMatch(`export { A } from "./dir/src-file"`);
+    });
+
+      test(`(exclude) Doesn't transform for exclusion patterns`, () => {
+        expect(rootDirsEmit[indexFile].dts).toMatch(
+          /export { BB } from "#exclusion\/ex";\s*export { DD } from "#root\/excluded-file";/
+        );
+      });
+    });
+
+    describe(`Tags`, () => {
+      test(`(@no-transform-path) Doesn't transform path`, () => {
+        const regex = /^import \* as skipTransform\d from "#root\/index"/gm;
+        const expectedLength = tsInstance.versionMajorMinor === '3.6' ? 8 : 16;
+        const matches = [
+          ...(normalEmit[tagFile].dts.match(regex) ?? []),
+          ...(rootDirsEmit[tagFile].dts.match(regex) ?? []),
+          ...(normalEmit[tagFile].js.match(regex) ?? []),
+          ...(rootDirsEmit[tagFile].js.match(regex) ?? []),
+        ];
+        expect(matches).toHaveLength(expectedLength);
+      });
+
+      test(`(@transform-path) Transforms path with explicit value`, () => {
+        const regex1 = /^import \* as explicitTransform\d from "\.\/dir\/src-file"/gm;
+        const regex2 = /^import \* as explicitTransform\d from "http:\/\/www\.go\.com\/react\.js"/gm;
+        const expectedLength = tsInstance.versionMajorMinor === '3.6' ? 4 : 8;
+
+        const matches1 = [
+          ...(normalEmit[tagFile].dts.match(regex1) ?? []),
+          ...(rootDirsEmit[tagFile].dts.match(regex1) ?? []),
+          ...(normalEmit[tagFile].js.match(regex1) ?? []),
+          ...(rootDirsEmit[tagFile].js.match(regex1) ?? []),
+        ];
+        expect(matches1).toHaveLength(expectedLength);
+
+        const matches2 = [
+          ...(normalEmit[tagFile].dts.match(regex2) ?? []),
+          ...(rootDirsEmit[tagFile].dts.match(regex2) ?? []),
+          ...(normalEmit[tagFile].js.match(regex2) ?? []),
+          ...(rootDirsEmit[tagFile].js.match(regex2) ?? []),
+        ];
+        expect(matches2).toHaveLength(expectedLength);
+      });
     });
 
     test(`Does not resolve external modules`, () => {
@@ -65,13 +131,6 @@ describe(`Transformer -> Specific Cases`, () => {
       expect(normalEmit[indexFile].js).toMatch(
         /\/\/ comment 1\r?\n\s*\r?\n\/\*\r?\n\s*comment 2\r?\n\s*\*\/\r?\n\s*"\.\.\/generated\/dir\/gen-file"/
       );
-    });
-
-    test(`(useRootDirs: false) Ignores rootDirs`, () => {
-      expect(normalEmit[genFile].dts).toMatch(`import "../../src/dir/src-file"`);
-      expect(normalEmit[srcFile].dts).toMatch(`import "../../generated/dir/gen-file"`);
-      expect(normalEmit[indexFile].dts).toMatch(`export { B } from "../generated/dir/gen-file"`);
-      expect(normalEmit[indexFile].dts).toMatch(`export { A } from "./dir/src-file"`);
     });
   });
 });
