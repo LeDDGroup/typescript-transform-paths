2a9d135215dfd512f91d13d4b7acad5f3bbeffa3
Ron S <ron@nonara.com>
2021-06-28 14:34:32 -0400

    fix: Accommodate TS 4.4 paths pattern caching (fixes #114)

--- src/transformer.ts
+++ src/transformer.ts
@@ -28,6 +28,10 @@ export default function transformer(
 
     if (!pathsBasePath || !compilerOptions.paths) return (sourceFile: ts.SourceFile) => sourceFile;
 
+    const { configFile, paths } = compilerOptions;
+    // TODO - Remove typecast when tryParsePatterns is recognized (probably after ts v4.4)
+    const { tryParsePatterns } = tsInstance as any;
+
     const tsTransformPathsContext: TsTransformPathsContext = {
       compilerOptions,
       config,
@@ -43,6 +47,10 @@ export default function transformer(
       excludeMatchers: config.exclude?.map((globPattern) => new Minimatch(globPattern, { matchBase: true })),
       parsedCommandLine: createParsedCommandLineForProgram(tsInstance, program),
       outputFileNamesCache: new Map(),
+      pathsPatterns: tryParsePatterns
+        // TODO - Remove typecast when pathPatterns is recognized (probably after ts v4.4)
+        ? (configFile?.configFileSpecs as any)?.pathPatterns || tryParsePatterns(paths)
+        : paths
     };
 
     return (sourceFile: ts.SourceFile) => {
