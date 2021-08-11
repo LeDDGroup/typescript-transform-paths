e602ee829eaafe9a3353c156f21f3e336112d76e
Ron S <ron@nonara.com>
2021-08-10 20:17:31 -0400

    refactor(register): Clarify error message language

--- register.js
+++ register.js
@@ -1,9 +1,11 @@
 let tsNode;
 try {
   tsNode = require('ts-node');
-} catch {}
-
-if (!tsNode) throw new Error(`Cannot register transformer without ts-node.`);
+} catch {
+  throw new Error(
+    `Cannot resolve ts-node. Make sure ts-node is installed before using typescript-transform-paths/register`
+  );
+}
 
 tsNode.register();
 require('./').register();
