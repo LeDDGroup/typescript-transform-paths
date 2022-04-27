5990bf9fd373b6c6175ae7bbe5a2f06357324ca1
zhaoyunfeng <zhaoyunfeng@worktile.com>
2022-04-27 17:51:04 +0800

    test: add e2e test for nx

---
+++ test/projects/nx/packages/library1/src/index.ts
@@ -0,0 +1,4 @@
+import { name as library2Name } from 'library2'
+export default library2Name;
+
+console.log(library2Name)
--- test/tests/extras.test.ts
+++ test/tests/extras.test.ts
@@ -1,26 +1,27 @@
-import { createTsProgram, getEmitResultFromProgram } from "../utils";
-import { projectsPaths } from "../config";
-import path from "path";
-import ts from "typescript";
-import * as config from "../config";
-import { execSync } from "child_process";
+import { createTsProgram, getEmitResultFromProgram } from '../utils';
+import { projectsPaths } from '../config';
+import path from 'path';
+import ts from 'typescript';
+import * as config from '../config';
+import { execSync } from 'child_process';
+import { rmSync, readFileSync } from 'fs';
 
 /* ****************************************************************************************************************** *
  * Tests
  * ****************************************************************************************************************** */
 
 describe(`Extra Tests`, () => {
-  const projectRoot = ts.normalizePath(path.join(projectsPaths, "extras"));
-  const indexFile = ts.normalizePath(path.join(projectRoot, "src/index.ts"));
-  const tsConfigFile = ts.normalizePath(path.join(projectRoot, "tsconfig.json"));
+  const projectRoot = ts.normalizePath(path.join(projectsPaths, 'extras'));
+  const indexFile = ts.normalizePath(path.join(projectRoot, 'src/index.ts'));
+  const tsConfigFile = ts.normalizePath(path.join(projectRoot, 'tsconfig.json'));
 
   describe(`Built Tests`, () => {
     // see: https://github.com/LeDDGroup/typescript-transform-paths/issues/130
     test(`Transformer works without ts-node being present`, () => {
       jest.doMock(
-        "ts-node",
+        'ts-node',
         () => {
-          require("sdf0s39rf3333d@fake-module");
+          require('sdf0s39rf3333d@fake-module');
         },
         { virtual: true }
       );
@@ -28,14 +29,27 @@ describe(`Extra Tests`, () => {
         const program = createTsProgram({ tsInstance: ts, tsConfigFile }, config.builtTransformerPath);
         const res = getEmitResultFromProgram(program);
         expect(res[indexFile].js).toMatch(`var _identifier_1 = require("./id")`);
-      } finally {
-        jest.dontMock("ts-node");
+      }
+      finally {
+        jest.dontMock('ts-node');
       }
     });
 
     test(`Register script transforms with ts-node`, () => {
-      const res = execSync("ts-node src/index.ts", { cwd: projectRoot }).toString();
+      const res = execSync('ts-node src/index.ts', { cwd: projectRoot }).toString();
       expect(res).toMatch(/^null($|\r?\n)/);
     });
+  });
+});
+
+describe(`Extra Tests: NX`, () => {
+  const projectRoot = ts.normalizePath(path.join(projectsPaths, 'nx'));
+
+  test(`Transformer works for emitted declaration`, () => {
+    execSync('yarn run build', { cwd: projectRoot })
+    const file = readFileSync(`${projectRoot}/dist/library1/packages/library1/src/index.d.ts`, 'utf8')
+    expect(file).toMatch(/import { name as library2Name } from "..\/..\/library2\/src";/)
+
+    rmSync(`${projectRoot}/dist`, { recursive: true, force: true })
   });
 });
