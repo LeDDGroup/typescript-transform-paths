51777a215659716310399f53960feb54af50b22c
Ron S <ron@nonara.com>
2022-10-21 13:49:06 -0400

    style: formatting

--- test/projects/specific/src/sub-packages.ts
+++ test/projects/specific/src/sub-packages.ts
@@ -12,4 +12,4 @@ export { subPackageConst as C4 } from "#packages/pkg-a/sub-pkg/main";
 // This path should resolve to './packages/pkg-a/sub-pkg/main.js', due to explicit extension
 export { subPackageConst as C5 } from "#packages/pkg-a/sub-pkg/main.js";
 
-export type ImportWithChildren = import("#packages/pkg-a").PassThru<import("#packages/pkg-b").PackageBType>
+export type ImportWithChildren = import("#packages/pkg-a").PassThru<import("#packages/pkg-b").PackageBType>;
