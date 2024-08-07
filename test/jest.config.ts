338dc874d83f1b718e92c261934db06c441bca18
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-07 16:08:39 +0000

    refact: tsconfig extends @tsconfig/bases (#220)

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -5,16 +5,6 @@ const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testRegex: ".*(test|spec)\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.json",
-      },
-    ],
-  },
-  modulePaths: ["<rootDir>"],
-  roots: ["<rootDir>"],
 };
 
 export default config;
