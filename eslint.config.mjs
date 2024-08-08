b008d8149dfcfda132b6b3f45399df818260e78c
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 02:06:29 -0400

    WIP ignore failing rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,34 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["**/dist/", "src/declarations/"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-empty": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "no-case-declarations": "off",
+      "no-useless-escape": "off",
+      "prefer-const": "off",
+      "prefer-rest-params": "off",
+      "prefer-spread": "off",
+    },
+  },
 ];
