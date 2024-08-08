f2ffa3b40b23f72f55bdf099b46f6e2bea926da9
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 11:16:37 -0400

    don't lint types/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["**/dist/", "src/declarations/", "test/projects/"] },
+  { ignores: ["**/dist/", "src/declarations/", "test/projects/", "types/"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
