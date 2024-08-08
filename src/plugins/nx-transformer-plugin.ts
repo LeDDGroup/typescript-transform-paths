f04ac777a374c30fe5d190e1322a23dee28ccb56
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 22:25:20 +0000

    refactor: remove hardcoded types (#240)

--- src/plugins/nx-transformer-plugin.ts
+++ src/plugins/nx-transformer-plugin.ts
@@ -1,6 +1,23 @@
-import { NxTransformerPlugin } from "../../types";
 import transformer from "../transformer";
 import ts from "typescript";
+
+export interface TsTransformPathsConfig {
+  readonly useRootDirs?: boolean;
+  readonly exclude?: string[];
+  readonly afterDeclarations?: boolean;
+  readonly tsConfig?: string;
+  readonly transform?: string;
+}
+
+export type NxTransformerFactory = (
+  config?: Omit<TsTransformPathsConfig, "transform">,
+  program?: ts.Program,
+) => ts.TransformerFactory<ts.SourceFile>;
+
+export interface NxTransformerPlugin {
+  before: NxTransformerFactory;
+  afterDeclarations: NxTransformerFactory;
+}
 
 /* ****************************************************************************************************************** *
  * Locals
