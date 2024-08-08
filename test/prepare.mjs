de632f92f2c6449466bc39d523c5531fe8725209
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 00:30:02 -0400

    add comment explaining why we need to symlink

--- test/prepare.mjs
+++ test/prepare.mjs
@@ -10,6 +10,9 @@ import { patch as patch2 } from "tsp2";
 const __dirname = dirname(fileURLToPath(import.meta.url)); // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
 
 async function symlinkTsNode() {
+  // we need to patch the root package node_modules in order for it to locate ts-node for the register tests.
+  // installing ts-node as a depenency in the root is not an option since it would create two copies of ts-node
+  // thus messing with the mocks in the tests
   const target = resolve(__dirname, "node_modules/ts-node");
   const path = resolve(__dirname, "../node_modules/ts-node");
 
