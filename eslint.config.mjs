0e9f604d16c6e6629877ba13a0857feea26e92be
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 14:00:27 -0400

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["**/dist/", "test/projects/", "types/"] },
+  { ignores: ["**/dist/", "test/projects/"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
