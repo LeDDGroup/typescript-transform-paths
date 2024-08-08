5dd9c3fc8491a9e9375700f699665d999cf65340
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 19:34:42 -0400

    WIP fix types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import transformer from "./transformer";
 export default transformer;
 
-export { TsTransformPathsConfig } from "./types";
+export type { TsTransformPathsConfig } from "./types";
 export { register } from "./register";
 export { nxTransformerPlugin } from "./plugins";
