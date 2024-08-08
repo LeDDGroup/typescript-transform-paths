6f8c3218c44bd052868a0706017f497318782d8c
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-08 15:27:24 +0000

    refactor: remove namespaces in favor of modules (#236)
    Removing typescript only namespaces in favor of using modules per
    typescript recommendation. This should produce more compliant code and
    output more readable javascript
    
    [From
    typescript](https://www.typescriptlang.org/docs/handbook/namespaces-and-modules.html#using-modules)
    
    > It is also worth noting that, for Node.js applications, modules are
    the default and we recommended modules over namespaces in modern code.
    
    > Starting with ECMAScript 2015, modules are native part of the
    language, and should be supported by all compliant engine
    implementations. Thus, for new projects modules would be the recommended
    code organization mechanism.
    
    See [the
    diff](https://github.com/LeDDGroup/typescript-transform-paths/pull/236/files?diff=unified&w=1)
    without whitespace changes

--- src/harmony/versions/four-seven.ts
+++ src/harmony/versions/four-seven.ts
@@ -21,20 +21,18 @@ import { DownSampleTsTypes } from "../utils";
 // region: Mapping
 /* ****************************************************************************************************************** */
 
-export namespace TsFourSeven {
-  export type TypeMap = [
-    [TsCurrentModule.ImportDeclaration, TsFourSevenModule.ImportDeclaration],
-    [TsCurrentModule.Modifier, TsFourSevenModule.Modifier],
-    [TsCurrentModule.ImportClause, TsFourSevenModule.ImportClause],
-    [TsCurrentModule.Expression, TsFourSevenModule.Expression],
-    [TsCurrentModule.AssertClause, TsFourSevenModule.AssertClause],
-    [TsCurrentModule.ExportDeclaration, TsFourSevenModule.ExportDeclaration],
-    [TsCurrentModule.NamedExportBindings, TsFourSevenModule.NamedExportBindings],
-    [TsCurrentModule.ModuleDeclaration, TsFourSevenModule.ModuleDeclaration],
-    [TsCurrentModule.ModuleName, TsFourSevenModule.ModuleName],
-    [TsCurrentModule.ModuleBody, TsFourSevenModule.ModuleBody],
-  ];
-}
+export type TypeMap = [
+  [TsCurrentModule.ImportDeclaration, TsFourSevenModule.ImportDeclaration],
+  [TsCurrentModule.Modifier, TsFourSevenModule.Modifier],
+  [TsCurrentModule.ImportClause, TsFourSevenModule.ImportClause],
+  [TsCurrentModule.Expression, TsFourSevenModule.Expression],
+  [TsCurrentModule.AssertClause, TsFourSevenModule.AssertClause],
+  [TsCurrentModule.ExportDeclaration, TsFourSevenModule.ExportDeclaration],
+  [TsCurrentModule.NamedExportBindings, TsFourSevenModule.NamedExportBindings],
+  [TsCurrentModule.ModuleDeclaration, TsFourSevenModule.ModuleDeclaration],
+  [TsCurrentModule.ModuleName, TsFourSevenModule.ModuleName],
+  [TsCurrentModule.ModuleBody, TsFourSevenModule.ModuleBody],
+];
 
 // endregion
 
@@ -42,83 +40,81 @@ export namespace TsFourSeven {
 // region: Utils
 /* ****************************************************************************************************************** */
 
-export namespace TsFourSeven {
-  export const predicate = ({ tsVersionMajor, tsVersionMinor }: TsTransformPathsContext) =>
-    tsVersionMajor == 4 && tsVersionMinor < 8;
+export const predicate = ({ tsVersionMajor, tsVersionMinor }: TsTransformPathsContext) =>
+  tsVersionMajor == 4 && tsVersionMinor < 8;
 
-  export function handler(context: TsTransformPathsContext, prop: string | symbol) {
-    const factory = context.tsFactory as unknown as TsFourSevenModule.NodeFactory;
+export function handler(context: TsTransformPathsContext, prop: string | symbol) {
+  const factory = context.tsFactory as unknown as TsFourSevenModule.NodeFactory;
 
-    switch (prop) {
-      case "updateImportDeclaration":
-        return function (
-          node: ImportDeclaration,
-          modifiers: readonly Modifier[] | undefined,
-          importClause: ImportClause | undefined,
-          moduleSpecifier: Expression,
-          assertClause: AssertClause | undefined,
-        ) {
-          const [dsNode, dsImportClause, dsModuleSpecifier, dsAssertClause] = downSample(
-            node,
-            importClause,
-            moduleSpecifier,
-            assertClause,
-          );
+  switch (prop) {
+    case "updateImportDeclaration":
+      return function (
+        node: ImportDeclaration,
+        modifiers: readonly Modifier[] | undefined,
+        importClause: ImportClause | undefined,
+        moduleSpecifier: Expression,
+        assertClause: AssertClause | undefined,
+      ) {
+        const [dsNode, dsImportClause, dsModuleSpecifier, dsAssertClause] = downSample(
+          node,
+          importClause,
+          moduleSpecifier,
+          assertClause,
+        );
 
-          return factory.updateImportDeclaration(
-            dsNode,
-            dsNode.decorators,
-            dsNode.modifiers,
-            dsImportClause,
-            dsModuleSpecifier,
-            dsAssertClause,
-          );
-        };
-      case "updateExportDeclaration":
-        return function (
-          node: ExportDeclaration,
-          modifiers: readonly Modifier[] | undefined,
-          isTypeOnly: boolean,
-          exportClause: NamedExportBindings | undefined,
-          moduleSpecifier: Expression | undefined,
-          assertClause: AssertClause | undefined,
-        ) {
-          const [dsNode, dsExportClause, dsModuleSpecifier, dsAssertClause] = downSample(
-            node,
-            exportClause,
-            moduleSpecifier,
-            assertClause,
-          );
+        return factory.updateImportDeclaration(
+          dsNode,
+          dsNode.decorators,
+          dsNode.modifiers,
+          dsImportClause,
+          dsModuleSpecifier,
+          dsAssertClause,
+        );
+      };
+    case "updateExportDeclaration":
+      return function (
+        node: ExportDeclaration,
+        modifiers: readonly Modifier[] | undefined,
+        isTypeOnly: boolean,
+        exportClause: NamedExportBindings | undefined,
+        moduleSpecifier: Expression | undefined,
+        assertClause: AssertClause | undefined,
+      ) {
+        const [dsNode, dsExportClause, dsModuleSpecifier, dsAssertClause] = downSample(
+          node,
+          exportClause,
+          moduleSpecifier,
+          assertClause,
+        );
 
-          return factory.updateExportDeclaration(
-            dsNode,
-            dsNode.decorators,
-            dsNode.modifiers,
-            isTypeOnly,
-            dsExportClause,
-            dsModuleSpecifier,
-            dsAssertClause,
-          );
-        };
-      case "updateModuleDeclaration":
-        return function (
-          node: ModuleDeclaration,
-          modifiers: readonly Modifier[] | undefined,
-          name: ModuleName,
-          body: ModuleBody | undefined,
-        ) {
-          const [dsNode, dsName, dsBody] = downSample(node, name, body);
+        return factory.updateExportDeclaration(
+          dsNode,
+          dsNode.decorators,
+          dsNode.modifiers,
+          isTypeOnly,
+          dsExportClause,
+          dsModuleSpecifier,
+          dsAssertClause,
+        );
+      };
+    case "updateModuleDeclaration":
+      return function (
+        node: ModuleDeclaration,
+        modifiers: readonly Modifier[] | undefined,
+        name: ModuleName,
+        body: ModuleBody | undefined,
+      ) {
+        const [dsNode, dsName, dsBody] = downSample(node, name, body);
 
-          return factory.updateModuleDeclaration(dsNode, dsNode.decorators, dsNode.modifiers, dsName, dsBody);
-        };
-      default:
-        return (...args: any) => (<any>factory)[prop](...args);
-    }
+        return factory.updateModuleDeclaration(dsNode, dsNode.decorators, dsNode.modifiers, dsName, dsBody);
+      };
+    default:
+      return (...args: any) => (<any>factory)[prop](...args);
   }
+}
 
-  export function downSample<T extends [...unknown[]]>(...args: T): DownSampleTsTypes<TypeMap, T> {
-    return <any>args;
-  }
+export function downSample<T extends [...unknown[]]>(...args: T): DownSampleTsTypes<TypeMap, T> {
+  return <any>args;
 }
 
 // endregion
