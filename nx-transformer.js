322ad7771d6564136675ce8f534a787601370162
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-17 22:04:28 -0400

    chore!: remove main and types package.json properties

--- nx-transformer.js
+++
@@ -1,6 +0,0 @@
-// Keeping register here in the root for backwards compatibiliy, TODO remove in the next major version
-console.warn(
-  "typescript-transform-paths: Calling the top level nx-transformer file is deprecated and will be removed in the future. Use a tool that supports package.json exports",
-);
-
-module.exports = require("./dist/plugins/nx-transfomer-plugin").default;
