9917058b2093ba498a732b03b99dd15641c94657
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-09-02 20:32:09 -0400

    chore!: remove typescript 3 support

--- test/prepare.mjs
+++ test/prepare.mjs
@@ -4,7 +4,6 @@ import { symlink } from "node:fs/promises";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { patch } from "ts-patch";
-import { patch as patch1 } from "tsp1";
 import { patch as patch2 } from "tsp2";
 
 const __dirname = dirname(fileURLToPath(import.meta.url)); // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
@@ -28,7 +27,6 @@ function patchTsModules() {
     tspatch(["tsc.js", "typescript.js"], { basedir, dir: basedir });
   }
 
-  patchTypescript("typescript-3", patch1);
   patchTypescript("typescript-4.7", patch2);
   patchTypescript("typescript-5.5", patch);
   patchTypescript("typescript-5.6", patch);
