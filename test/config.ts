d574a592cf543c318a659f3949b7f64f4181bee5
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-09-14 10:49:45 -0400

    chore!: remove typescript 4.7 support

--- test/config.ts
+++ test/config.ts
@@ -1,11 +1,7 @@
 import ts from "typescript";
-import tsFourSeven from "typescript-4.7";
 import path from "node:path";
 
-export const tsModules = <const>[
-  ["4.7.4", tsFourSeven, "typescript-4.7"],
-  ["Latest", ts, "typescript"],
-];
+export const tsModules = <const>[["Latest", ts, "typescript"]];
 
 export const projectsPaths = path.join(__dirname, "./projects");
 export const transformerPath = require.resolve("typescript-transform-paths");
