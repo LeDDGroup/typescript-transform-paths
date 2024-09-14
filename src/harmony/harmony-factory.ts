f877740d5163d10cc100b7c8aef193f1e4b15236
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-09-14 10:42:26 -0400

    remove harmony code

--- src/harmony/harmony-factory.ts
+++
@@ -1,9 +0,0 @@
-import TS from "typescript";
-import { TsTransformPathsContext } from "../types";
-
-export interface HarmonyFactory extends TS.NodeFactory {}
-
-/** Creates a node factory compatible with TS v3+ */
-export function createHarmonyFactory(context: TsTransformPathsContext): HarmonyFactory {
-  return (context.tsFactory ?? context.tsInstance) as HarmonyFactory;
-}
