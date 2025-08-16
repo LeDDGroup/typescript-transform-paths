f180d15423afc207a0ded0850b8d59aa720db3c0
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-08-16 16:12:23 -0700

    WIP migrating to node test

--- test/tests/extras.test.ts
+++ test/tests/extras.test.ts
@@ -1,9 +1,12 @@
-import { createTsProgram, getEmitResultFromProgram, ModuleNotFoundError } from "../utils";
-import { projectsPaths } from "../config";
+import escapeStringRegexp from "escape-string-regexp";
+import { execSync } from "node:child_process";
 import path from "node:path";
+import test, { describe } from "node:test";
 import ts from "typescript";
 import * as config from "../config";
-import { execSync } from "node:child_process";
+import { projectsPaths } from "../config";
+import { createTsProgram, getEmitResultFromProgram, ModuleNotFoundError } from "../utils";
+import { stripVTControlCharacters } from "node:util";
 
 /* ****************************************************************************************************************** *
  * Tests
@@ -16,38 +19,33 @@ describe(`Extra Tests`, () => {
 
   describe(`Built Tests`, () => {
     // see: https://github.com/LeDDGroup/typescript-transform-paths/issues/130
-    test(`Transformer works without ts-node being present`, () => {
-      jest.doMock(
-        "ts-node",
-        () => {
-          throw new ModuleNotFoundError("ts-node");
-        },
-        { virtual: true },
-      );
-      try {
-        const program = createTsProgram({ tsInstance: ts, tsConfigFile }, config.builtTransformerPath);
-        const res = getEmitResultFromProgram(program);
-        expect(res[indexFile].js).toMatch(`var _identifier_1 = require("./id")`);
-      } finally {
-        jest.dontMock("ts-node");
-      }
-    });
+    // test(`Transformer works without ts-node being present`, (t) => {
+    //   jest.doMock(
+    //     "ts-node",
+    //     () => {
+    //       throw new ModuleNotFoundError("ts-node");
+    //     },
+    //     { virtual: true },
+    //   );
+    //   try {
+    //     const program = createTsProgram({ tsInstance: ts, tsConfigFile }, config.builtTransformerPath);
+    //     const res = getEmitResultFromProgram(program);
+    //     t.assert.match(res[indexFile].js, new RegExp(escapeStringRegexp(`var _identifier_1 = require("./id")`)));
+    //     expect(res[indexFile].js).toMatch(`var _identifier_1 = require("./id")`);
+    //   } finally {
+    //     jest.dontMock("ts-node");
+    //   }
+    // });
 
     describe(`ts-node register script`, () => {
-      /** Yarn sometimes outputs bold text, which makes these tests flakey */
-      function stripAnsi(str: string) {
-        // eslint-disable-next-line no-control-regex
-        return str.replace(/\x1B\[[0-?]*[ -/]*[@-~]/g, "");
-      }
-
-      test(`Works with --transpileOnly`, () => {
+      test(`Works with --transpileOnly`, (t) => {
         const res = execSync("yarn g:ts-node --transpileOnly src/index.ts", { cwd: projectRoot }).toString();
-        expect(stripAnsi(res.trim())).toEqual("null");
+        t.assert.equal(stripVTControlCharacters(res.trim()), "null");
       });
 
-      test(`Works with --typeCheck`, () => {
+      test(`Works with --typeCheck`, (t) => {
         const res = execSync("yarn g:ts-node --typeCheck src/index.ts", { cwd: projectRoot }).toString();
-        expect(stripAnsi(res.trim())).toEqual("null");
+        t.assert.equal(stripVTControlCharacters(res.trim()), "null");
       });
     });
   });
