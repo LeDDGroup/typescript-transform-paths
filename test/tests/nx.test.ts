d2092db3b18fdd2b20eeae9936bba540b211508e
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-09-05 00:00:03 -0700

    remove skipped tests

--- test/tests/nx.test.ts
+++ test/tests/nx.test.ts
@@ -7,60 +7,7 @@ import ts from "typescript";
 
 import { projectsPaths } from "../config.ts";
 
-// import * as nxTransformerPlugin from "typescript-transform-paths/plugins/nx";
-// import * as transformerModule from "../../dist/transformer";
-
-/* ****************************************************************************************************************** *
- * Tests
- * ****************************************************************************************************************** */
-
 describe(`NX Transformer`, () => {
-  // TODO migrate test from jest to node test runner
-  // describe("Plugin", () => {
-  //   let mockedTransformer: jest.SpyInstance;
-
-  //   const program = { x: 1 };
-
-  //   before(async () => {
-  //     // @ts-expect-error TS(2345) FIXME: Argument of type '() => void' is not assignable to parameter of type '(transformationContext: TransformationContext) => (sourceFile: SourceFile) => SourceFile'.
-  //     mockedTransformer = jest.spyOn(transformerModule, "default").mockReturnValue(() => {});
-  //   });
-  //   after(() => {
-  //     mockedTransformer.mockClear();
-  //   });
-  //   beforeEach(() => {
-  //     mockedTransformer.mockReset();
-  //   });
-
-  //   test(`Before properly routes transform`, () => {
-  //     const config = { a: 2 };
-
-  //     // @ts-expect-error TS(2559) FIXME: Type '{ a: number; }' has no properties in common with type 'Omit<TsTransformPathsConfig, "transform">'.
-  //     nxTransformerPlugin.before(config, program);
-
-  //     expect(mockedTransformer).toHaveBeenCalledTimes(1);
-  //     expect(mockedTransformer.mock.lastCall).toHaveLength(2);
-
-  //     const [recProgram, recConfig] = mockedTransformer.mock.lastCall;
-  //     expect(recProgram).toBe(program);
-  //     expect(recConfig).toStrictEqual(config);
-  //   });
-
-  //   test(`After properly routes transform`, () => {
-  //     const config = { a: 2, afterDeclarations: true };
-
-  //     // @ts-expect-error TS(2345) FIXME: Argument of type '{ x: number; }' is not assignable to parameter of type 'Program'.
-  //     nxTransformerPlugin.afterDeclarations(config, program);
-
-  //     expect(mockedTransformer).toHaveBeenCalledTimes(1);
-  //     expect(mockedTransformer.mock.lastCall).toHaveLength(2);
-
-  //     const [recProgram, recConfig] = mockedTransformer.mock.lastCall;
-  //     expect(recProgram).toBe(program);
-  //     expect(recConfig).toStrictEqual({ ...config, afterDeclarations: true });
-  //   });
-  // });
-
   describe(`(e2e) Works in NX project`, () => {
     const projectRoot = ts.normalizePath(path.join(projectsPaths, "nx"));
 
