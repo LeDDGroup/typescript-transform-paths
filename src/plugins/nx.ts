597fe5c449e26cefabbb3f16c8aa11ca233a114b
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-09-04 23:16:09 -0700

    rename

--- src/plugins/nx.ts
+++ src/plugins/nx.ts
@@ -1,5 +1,9 @@
-import transformer from "../transformer";
+// See nx-transformer-plugin.ts
+// https://github.com/nrwl/nx/blob/229f71ef1758ee625869aaa6fa6355dc3284fa5b/packages/js/src/utils/typescript/types.ts#L19-L32
+// https://github.com/nrwl/nx/blob/master/packages/js/src/utils/typescript/load-ts-transformers.ts
 import ts from "typescript";
+
+import transformer from "../transformer.ts";
 
 export interface TsTransformPathsConfig {
   readonly useRootDirs?: boolean;
